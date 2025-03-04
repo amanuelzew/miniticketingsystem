0c96a758e79c5688b10eb6d8287c308fece0b984
amanzewdut <amanzewdut@gmail.com>
2025-03-04 16:04:54 +0000

    remove console log

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -8,8 +8,7 @@ const protect=asyncHandler(async (req,res,next)=>{
     let token;
 
     token=req.cookies.jwt
-    console.log(req,"qq")
-    console.log(req.cookies.jwt)
+   
     if(token){
         try{
             const decoded=jwt.verify(token,process.env.JWT_SECRET)
